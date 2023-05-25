4f39acb6656d53026326a8b3ee000b9298c386b4
LeonardoRichie <91533882+LeonardoRichie@users.noreply.github.com>
2023-05-26 00:17:26 +0700

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import Login from './components/Login';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes,useLocation, Navigate } from "react-router-dom";
 import Success from './components/Success';
 
+const setToken = (token) => {
+  localStorage.setItem('key', token);
+};
+const fetchToken = () => {
+  return localStorage.getItem('key');
+};
+
+function RequireToken({ children }) {
+  let auth = fetchToken();
+  let location = useLocation();
+
+  if (!auth) {
+    setToken('test');
+    return <Navigate to='/' state={{ from: location }} />;
+  }
+
+  return children;
+}
 
 
 const App = () => {
@@ -10,7 +28,14 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route exact path="/success" element={<Success />} />
+        <Route
+          path="/success"
+          element={
+            <RequireToken>
+              <Success />
+            </RequireToken>
+          }
+        />
       </Routes>
     </Router>
 
